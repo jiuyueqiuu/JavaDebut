378a93977776bf2892564b71ff2356c18acd388d
Haoran Yin <haoran.hry@gmail.com>
2025-11-17 16:20:58 +0100

    Mile stone : finished Java Random

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,21 @@
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        Random random = new Random();
 
-        Scanner scanner = new  Scanner(System.in);
+        boolean coin;
 
-        String name;
-        System.out.print("Enter your name: ");
-        name = scanner.next();
+        coin = random.nextBoolean();
 
-        if(name.length() < 3) {
-            System.out.println("Thy name is too short !");
+        if(coin) {
+            System.out.println("HEADS !");
         }
-        else {
-            System.out.println("Moin, " + name + " !");
-        }
-
-        int age;
-        System.out.print("Enter your age: ");
-        age = scanner.nextInt();
-
-        if(age >= 18) {
-            System.out.println("You are majeure !");
-        }
-        else if(age < 0) {
-            System.out.println("You arent even born yet bro !");
-        }
-        else {
-            System.out.println("You are mineure !");
-        }
-
-
-        boolean isEtudiant;
-        System.out.print("Art thou a student ? (true/false) ");
-        isEtudiant = scanner.nextBoolean();
-
-        if(isEtudiant) {
-            System.out.println("Very nice ! I wish you study well !");
-        }
-        else {
-            System.out.println("Hmmmm... Perhaps thou art working...");
+        else  {
+            System.out.println("TAILS !");
         }
     }
 }
