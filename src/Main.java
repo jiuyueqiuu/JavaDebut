9f84d0ecbc3ec562a2b155bf1ee5bd1569d50955
Haoran Yin <haoran.hry@gmail.com>
2025-11-17 18:04:55 +0100

    Mile stone : learned the enhanced switch

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,57 @@ import java.util.Random;
 // variable = (condition) ? ifTrue : if False;
 // Exemple : String evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
 
+
+
+/**  Enhanced Switch (Java14 feature)
+ * A replacement to many else if statements
+ String day;
+ System.out.print("Enter a day in the week : ");
+ day = scanner.nextLine();
+
+ switch (day) {
+ case "Monday" -> System.out.println("It is a weekday :( ");
+ case "Tuesday" -> System.out.println("It is a weekday :( ");
+ case "Wednesday" -> System.out.println("It is a weekday :( ");
+ case "Thursday" -> System.out.println("It is a weekday :( ");
+ case "Friday" -> System.out.println("It is a weekday :( ");
+ case "Saturday" -> System.out.println("It is a weekend :) ");
+ case "Sunday" -> System.out.println("It is a weekend :) ");
+ default -> System.out.println(day + " not exist lil bro");
+ }
+
+
+ OR
+
+ switch (day) {
+ case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ->
+ System.out.println("It is a weekday :( ");
+ case "Saturday", "Sunday" ->
+ System.out.println("It is a weekend :) ");
+ default -> System.out.println(day + " not exist lil bro");
+ }
+
+ */
+
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        double temp;
-        double newTemp;
-        String unit;
+        String day;
+        System.out.print("Enter a day in the week : ");
+        day = scanner.nextLine();
 
-        System.out.print("Enter the temperature : ");
-        temp = scanner.nextDouble();
+        switch (day) {
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ->
+                    System.out.println("It is a weekday :( ");
+            case "Saturday", "Sunday" ->
+                    System.out.println("It is a weekend :) ");
+            default -> System.out.println(day + " not exist lil bro");
+        }
 
-        System.out.print("Convert to Celsius or Fahrenheit ? (C or F) : ");
-        unit = scanner.next().toUpperCase();
 
-        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 5 / 9) + 32;
-
-
-        System.out.println(newTemp + " " + unit);
 
         scanner.close();
     }
